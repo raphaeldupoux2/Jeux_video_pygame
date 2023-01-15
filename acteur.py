5e780abcb377045a9c473e1d7d413654777ef8c2
zoouum <luddpx@gmail.com>
2023-01-15 23:23:49 +0100

    collision

--- acteur.py
+++ acteur.py
@@ -1,11 +1,13 @@
+import math
 from abc import ABC, abstractmethod
+from random import uniform
 
 import pygame
 
 
 class Acteur(ABC):
 
-    def __init__(self, game_instance, x, y, vel, sizeX, sizeY, couleur):
+    def __init__(self, game_instance, x, y, vel, sizeX, sizeY, couleur, solide):
         self.x: float = x
         self.y: float = y
         self.vel: float = vel
@@ -15,6 +17,8 @@ class Acteur(ABC):
         self.sizeX = sizeX
         self.sizeY = sizeY
         self.couleur = couleur
+        self.solide = solide
+        self.direction = uniform(0, 2 * math.pi)
 
     def limit(self):
         if self.x < 0:
@@ -36,6 +40,31 @@ class Acteur(ABC):
     def affiche(self):
         pygame.draw.rect(self.game_instance.win, self.couleur, (self.x, self.y, self.sizeX, self.sizeY))
 
-    def touche(self, acteur):
-        return self.x + self.sizeX > acteur.x > self.x - self.game_instance.player[0].sizeX and \
-            self.y + self.sizeY > acteur.y > self.y - self.game_instance.player[0].sizeY
+    def touche(self, acteur, marge=0):
+        return acteur.x < self.x + self.sizeX + marge and self.x - marge < acteur.x + acteur.sizeX and \
+            acteur.y < self.y + self.sizeY + marge and self.y - marge < acteur.y + acteur.sizeY
+
+    def _bouge(self):
+        self.x += math.cos(self.direction) * self.vel
+        self.y += math.sin(self.direction) * self.vel
+
+    def bouge(self):
+        old_x, old_y = self.x, self.y
+        self._bouge()
+        if not self.solide:
+            return
+
+        for sol in filter(lambda x: x.solide, self.game_instance.acteurs):
+            if sol != self and sol.touche(self):
+                new_x = self.x
+                self.x = old_x
+                if not sol.touche(self):
+                    continue
+
+                self.x = new_x
+                self.y = old_y
+                if not sol.touche(self):
+                    continue
+
+                self.x = old_x
+                break
--- buisson.py
+++ buisson.py
@@ -5,7 +5,7 @@ from acteur import Acteur
 
 class Buisson(Acteur):
     def __init__(self, game_instance):
-        super().__init__(game_instance, randint(300, 1000), randint(200, 450), 0, 80, 40, (0, 255, 0))
+        super().__init__(game_instance, randint(300, 1000), randint(200, 450), 0, 80, 40, (0, 255, 0), False)
 
     def hide(self, acteur_affecte: list):
         for acteur in acteur_affecte:
